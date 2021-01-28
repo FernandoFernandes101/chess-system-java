12bc01408f9b4863d014c9a996f0597ede42c22e
fernandoFernandes101 <f_fernando@hotmail.com>
2021-01-28 09:14:26 -0300

    Little improvement on boar printiin board printing

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package Chess;
 
 import Chess.pieces.King;
-
 import Chess.pieces.Rook;
 import boardgame.Board;
 
@@ -30,9 +29,19 @@ public class ChessMatch {
 	
 	
 	public void initialSetup() {
-		placeNewPiece('a', 3, new Rook(board, Color.WHITE));
-		placeNewPiece('d',4, new King(board, Color.BLACK));
-		placeNewPiece('c', 6, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 }
 
